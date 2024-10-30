d1b2646394bf76f739cf10e76dcec39a09cd0d9a
daniyalzia332 <daniyal.zia@hotmail.com>
2024-10-30 04:24:24 -0700

    all

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector(form);
+
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
+const obj = document.createElement("ani-image.gif");
+obj.loop = true; // true
